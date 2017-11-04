f00ece4a5d6dd802ff2087dca08d7f3d44a61cf7
Jesus Villarruel <jesusvillarruel@student.hartnell.edu>
2017-11-04 14:37:10 -0700

    Adding the class required for the medical details page

---
+++ help.me/app/src/main/java/com/firstapp/helpme/MedicalDetailsActivity.java
@@ -0,0 +1,17 @@
+package com.firstapp.helpme;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MedicalDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicaldetails);
+    }
+
+}
