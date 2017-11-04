d21c53b9a706dc0c7de9a2a85a4516533c2ca180
Jesus Villarruel <jesusvillarruel@student.hartnell.edu>
2017-11-04 15:43:54 -0700

    Added connectivity to link it to the medical details activity

--- help.me/app/src/main/java/com/firstapp/helpme/ResourcesActivity.java
+++ help.me/app/src/main/java/com/firstapp/helpme/ResourcesActivity.java
@@ -1,7 +1,10 @@
 package com.firstapp.helpme;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ResourcesActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class ResourcesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources);
+    }
+
+    public void resourcesButtonClicked(View v){
+        startActivity(new Intent(ResourcesActivity.this, MedicalDetailsActivity.class));
     }
 }
