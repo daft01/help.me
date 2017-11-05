a5509fdf2404113f623304c801ce32e1d54bed06
Daniel Morales <danielmorales0000@yahoo.com>
2017-11-04 17:09:35 -0700

    Added sound effects to 'yes' 'no' buttons

--- help.me/app/src/main/java/com/firstapp/helpme/LifeThreateningAmergency.java
+++ help.me/app/src/main/java/com/firstapp/helpme/LifeThreateningAmergency.java
@@ -1,15 +1,30 @@
 package com.firstapp.helpme;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LifeThreateningAmergency extends AppCompatActivity {
+
+    private SoundPool mSoundPool;
+    private int clickButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life_threatening_amergency);
+        mSoundPool = new SoundPool(1, AudioManager.STREAM_MUSIC,0);
+        clickButton = mSoundPool.load(getApplicationContext(), R.raw.click, 1);
     }
 
+    public void yesButtonClicked(View v){
+        mSoundPool.play(clickButton, 1.0f,1.0f, 0, 0, 1.0f);
+    }
+
+    public void noButtonClicked(View v){
+        mSoundPool.play(clickButton, 1.0f,1.0f, 0, 0, 1.0f);
+    }
 
 }
